4793d890ea5205e9dcfa724d30f04741b41fda54
= <=>
2019-03-06 19:28:10 -0800

    Almost done, fixing an issue with control-c

--- warn.c
+++ warn.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <ucontext.h>
 #include <unistd.h>
@@ -29,6 +30,7 @@ int pipeReee(int fileDescriptor, char* buffer, int size){
         }
         return errorHandler("No string read in?!?",errno);
     }else{
+
         if(fclose(readme) == EOF){
             return errorHandler("Failed to close file Stream",errno);
         }
@@ -42,7 +44,6 @@ void sigIntHandler(int signal){
     sigaction(SIGINT,&s2,NULL);
 }
 void sigIntHandlerTwo(int signal){
-    ualarm(0,0);
     pflag = 1;
     struct sigaction s2;
     s2.sa_handler = sigIntHandlerTwo;
@@ -61,16 +62,6 @@ void sigUserHandler(int signal){
     sigaction(SIGFPE,&s3,NULL);
 }
 
-void sigCloseHandler(int signal){
-    closeFlag = 0;
-    alarm(0);
-    ualarm(1,1); // Smash through any pauses in the while loop.
-    struct sigaction s3;
-    s3.sa_handler = sigUserHandler;
-    sigaction(31,&s3,NULL);
-}
-
-
 int pipeWrite(int fileDescriptor, char* buffer){
     int newfd = dup(fileDescriptor);
     if(dup < 0){
@@ -85,77 +76,108 @@ int pipeWrite(int fileDescriptor, char* buffer){
     return 0;
 }   
 
+// I aplogize for the mess, I didn't understand the assignment well enough to split it into pieces before I started.
+
 int main(){
-    char buffer[512] = "";
+    char buffer[2048] = "";
+    char buffer2[2048] = "";
     struct sigaction s1;
     struct sigaction s2;
     struct sigaction s3;
-    struct sigaction s4;
     struct sigaction s5;
     // fd[0] -> read
     // fd[1] -> write
     s1.sa_handler = sigAlarmHandler;
-    s2.sa_handler = sigIntHandler;
+    s2.sa_handler = sigIntHandlerTwo;
     s3.sa_handler = sigUserHandler;
-    s4.sa_handler = sigCloseHandler;
     s5.sa_handler = sigIntHandlerTwo;
     if(pipe(fd) < 0){
         return 1;
     }
     int res = fork();
     if(res < 0) return errorHandler("Failed to create child process",errno);
+    int waitTime = 5;
     if(!res){
-        int waitTime = 5;
+        char* word;
+        int ex = 0;
         sigaction(SIGALRM,&s1,NULL);
         sigaction(SIGINT,&s5,NULL);
         sigaction(SIGFPE,&s3,NULL);
         if(close(fd[1]) < 0) return errorHandler("Failed to close file descriptor",errno);
-        while(closeFlag){
+        while(1){
             if(pflag){
+                alarm(0);
                 pause();
-                pipeReee(fd[0],buffer,511);
-                // Used %[\001-\377] because %s wouldn't get the whole string >:I
-                if(sscanf(buffer,"%d %[\001-\377]\n",&waitTime,buffer) != 2){
-                    sscanf(buffer,"%[\001-\377]\n",buffer);
-                    waitTime = 5;
+                if(!pflag){
+                    printf("\n");
                 }
-                if(!strcmp("exit\n",buffer)) {
-                    printf("taking out the parent\n");
-                    kill(res,31);   // Tell the parent to kill itself.
-                    break;
-                    //kill(res,SIGKILL);
+                while(pflag){
+                    pflag = 0;
+                    pipeReee(fd[0],buffer,512);
+                    strncpy(buffer2,buffer,512);
+                    // Used %[\001-\377] because %s wouldn't get the whole string >:I
+                    if(sscanf(buffer,"%d",&waitTime) == 1){
+                        word = strtok(buffer," ");
+                        word = strtok(NULL,"");
+                        if(word == NULL){
+                            waitTime = 5;
+                            word = buffer2;
+                        }
+                    }else{
+                        waitTime = 5;
+                        word = buffer2;
+                    } 
+                    if(!strcmp("exit\n",word)) {
+                        ex = 1;
+                        break;
+                        //kill(res,SIGKILL);
+                    }
                 }
-                pflag = 0;
-            }else{
-                if(closeFlag){
-                    alarm(waitTime);
-                    pause();
-                }else{
-                    break;
-                }
-                if(strlen(buffer) != 0){
-                    printf("%s",buffer);
-                }
+                
+            }
+            if(ex) break;
+            if(waitTime < 0) waitTime = -waitTime;
+            if(waitTime == 0) waitTime = 1;
+            alarm(waitTime);
+            pause();
+            if(strlen(buffer) != 0 && !pflag){
+                printf("%s",word);
             }
         }
         if(close(fd[0]) < 0) return errorHandler("Failed to close file descriptor",errno);
+        printf("Exiting\n");
+        exit(0);
     }else{
         if(close(fd[0]) < 0) return errorHandler("Failed to close file descriptor",errno);
+
         sigaction(SIGINT,&s2,NULL);
-        sigaction(31,&s4,NULL);
         sigaction(SIGALRM,&s1,NULL);
-        while(closeFlag){
+        int len;
+        while(1){
             pause();
             printf("New string: ");
             if(fgets(buffer,512,stdin) != NULL){
+                while(getchar() != '\n');
+                len = strlen(buffer);
+                buffer[len-1] = '\n';
                 printf("\n");
                 pipeWrite(fd[1],buffer);
-                kill(res,SIGFPE);   // User defined signal.
+                kill(res,SIGFPE);
+                char* word = strtok(buffer," ");
+                word = strtok(NULL,"");
+                if(word == NULL){
+                    if(!strcmp("exit\n", buffer)) {
+                        break;
+                    }
+                }
+                else if(!strcmp("exit\n", word)) {
+                    break;
+                }
             }
         }
-    if(close(fd[1]) < 0) return errorHandler("Failed to close descriptor",errno);
-    //wait(NULL);
+
+        if(close(fd[1]) < 0) return errorHandler("Failed to close descriptor",errno);
+        wait(NULL);
     }
-    printf("Exiting\n");
     return 0;
 }
